5c9840e353098b5c9f417dae2f3b05c8301cc2d3
Arul Murugavel <63643748+BLooDBRothER@users.noreply.github.com>
2024-08-05 11:14:18 +0530

    Create rabbitmq to config factory

---
+++ src/rabbitMq/rabbitMq.ts
@@ -0,0 +1,42 @@
+import amqplib, { credentials } from "amqplib";
+import { error } from "console";
+
+const RabbitMQ = () => {
+    let _channel: amqplib.Channel ;
+
+    enum QUEUE {
+        "GROUP_CREATE" = "GROUP_CREATE"
+    }
+
+    const initConnection = async () => {
+        if(_channel) {
+            return;
+        }
+
+        if(!process.env.RABBITMQ_DEFAULT_USER || !process.env.RABBITMQ_DEFAULT_PASS) {
+            throw error("No credential for rabbitmq")
+        }
+
+        const connection = await amqplib.connect("amqp://localhost", {
+            credentials: credentials.plain(process.env.RABBITMQ_DEFAULT_USER, "admin"),
+        });
+
+        console.log(`[Info]: RabbitMQ | Connected...`);
+
+        _channel = await connection.createChannel();
+    }
+
+    const sendMessage = async (queue: string, msg: object) => {
+        await _channel.assertQueue(queue, {
+            durable: true
+        });
+
+        _channel.sendToQueue(queue, Buffer.from(JSON.stringify(msg)));
+    }
+
+    return { QUEUE, initConnection, sendMessage }
+};
+
+const rabbitMq = RabbitMQ();
+
+export default rabbitMq;
