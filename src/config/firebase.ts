6974950518e4b4bd47e4ad58b5f14bb45e7e1ef1
Arul Murugavel <63643748+BLooDBRothER@users.noreply.github.com>
2024-08-08 17:10:29 +0530

    refactor firebase initialize

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,13 +1,11 @@
 import admin, { ServiceAccount } from "firebase-admin";
 import firebaseServiceKey from "../firebase.json";
 
-const initializeFirebase = () => {
-    const firebaseAdminConfig = {
-        credential: admin.credential.cert(firebaseServiceKey as ServiceAccount),
-        firestore: process.env.DATABASE_URL,
-    };
-
-    admin.initializeApp(firebaseAdminConfig);
+const firebaseAdminConfig = {
+    credential: admin.credential.cert(firebaseServiceKey as ServiceAccount),
+    firestore: process.env.DATABASE_URL,
 };
 
-export default initializeFirebase;
+const firebaseApp = admin.initializeApp(firebaseAdminConfig);
+
+export default firebaseApp;
